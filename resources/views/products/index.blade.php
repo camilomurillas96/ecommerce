90ed7c095092471b1228a7e1ebb1f7f8a06f8b47
camilomurillas96 <camilogonzalez_96@hotmail.com>
2020-03-28 19:57:19 -0500

    Se modifica la vista index para mostrar los productos en una tabla

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -32,5 +32,9 @@
 
     </table>
 </div>
-
+<div class="floating">
+    <a href="{{url('/products/create')}}" class="btn btn-primary btn-fab">
+        <i class="material-icons">add</i>
+    </a>
+</div>
 @endsection
