f24dbb479838027666e9111290f09dcc27c4bb63
camilomurillas96 <camilogonzalez_96@hotmail.com>
2020-03-28 19:21:00 -0500

    Se crea la vista de productos

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="big-padding text-center blue-grey white-text">
+    <h1>Productos</h1>
+</div>
+<div class="container">
+    <table class="table table-bordered">
+        <thead>
+
+            <th>ID</th>
+            <th>Titulo</th>
+            <th>Descripcion</th>
+            <th>Precio</th>
+            <th>Acciones</th>
+
+        </thead>
+        <tbody>
+
+            @foreach ($products as $product)
+            <tr>
+                <td>{{$product->id}}</td>
+                <td>{{$product->title}}</td>
+                <td>{{$product->description}}</td>
+                <td>{{$product->pricing}}</td>
+                <td></td>
+            </tr>
+            @endforeach
+
+        </tbody>
+
+    </table>
+</div>
+
+@endsection
