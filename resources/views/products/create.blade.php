f55811fc703ac82bd87e23623403c7990e18e40a
camilomurillas96 <camilogonzalez_96@hotmail.com>
2020-03-28 20:34:04 -0500

    Se modifica la vista create para hacerla dinamica mediante variables y se incluye la vista form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -6,25 +6,7 @@
         <h1>Nuevo Producto</h1>
 
         {{-- formulario --}}
-    {!! Form::open(['url' => '/products', 'method' => 'POST'])!!}
-        <div class="form-group">
-            {{ Form::text('title','',['class' => 'form-control', 'placeholder' =>'Titulo...']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::text('pricing','',['class' => 'form-control', 'placeholder' =>'Precio de tu producto en dolares']) }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::text('description','',['class' => 'form-control', 'placeholder' =>'Describa el producto']) }}
-        </div>
-
-        <div class="form-group text-right">
-           <a href="{{url('/products')}}">Regresar al listado de productos</a>
-           <input type="submit" value="Enviar" class="btn btn-success">
-        </div>
-        
-    {!! Form::close() !!}
+        @include('products.form',['product' => $product, 'url' => '/products', 'method' => 'POST'])
 
     </div>
 
